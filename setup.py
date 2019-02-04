f021a09ff002e99fa96ac74c6dcbbd4c72d00b42
Tiago Vicente <tmavicente@gmail.com>
2019-02-04 20:33:49 +0000

    change any bugs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='get-real-flights',
-    version='0.1',
+    version='0.2',
     author="Tiago Vicente",
     author_email="tmavicente@gmail.com",
     description="A small script with real flights",
