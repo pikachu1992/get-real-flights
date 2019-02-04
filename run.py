a9837e4a4c8ff0db8dc1300fe177a3a8ea3f2078
Tiago Vicente <tmavicente@gmail.com>
2019-02-04 20:01:39 +0000

    remove while from run

--- run.py
+++ run.py
@@ -91,16 +91,15 @@ async def main():
     """
     
     airlines = ['TAP']
-    while True:
-        data = get_airlines.get_airlines()
-        for item in airlines:
-            for airline in data:           
-                if item == airline[0]:
-                    await get_flights_by_company(item)
-        with open('data/flight_radar.json', 'w') as file:
-            file.write(json.dumps(RESULT_LIST, indent=4))
-        make_route.make_route()
-        await asyncio.sleep(5)
+    data = get_airlines.get_airlines()
+    for item in airlines:
+        for airline in data:           
+            if item == airline[0]:
+                await get_flights_by_company(item)
+    with open('data/flight_radar.json', 'w') as file:
+        file.write(json.dumps(RESULT_LIST, indent=4))
+    make_route.make_route()
+    await asyncio.sleep(5)
 
 
 def get_start():
